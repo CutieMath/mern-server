46de38a21c857b3ad6463541b4510a118bcff217
Yuxin Ye <yuxin_ye@protonmail.com>
2022-11-22 15:53:57 +1100

    add file storage

--- index.js
+++ index.js
@@ -28,4 +28,7 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "public/assets");
   },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
 });
