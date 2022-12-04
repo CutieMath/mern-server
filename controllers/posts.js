c3d994a2c72fdacfe779bf178df26086d8a72df6
Yuxin Ye <yuxin_ye@protonmail.com>
2022-12-05 10:27:55 +1100

    add some bug fix

--- controllers/posts.js
+++ controllers/posts.js
@@ -28,20 +28,20 @@ export const createPost = async (req, res) => {
 
 export const getFeedPosts = async (req, res) => {
   try {
-    const posts = await Post.find();
-    res.status(200).json(posts);
+    const post = await Post.find();
+    res.status(200).json(post);
   } catch (err) {
-    res.status(404).json({ error: err.message });
+    res.status(404).json({ message: err.message });
   }
 };
 
 export const getUserPosts = async (req, res) => {
   try {
     const { userId } = req.params;
-    const posts = await Post.find({ userId });
-    res.status(200).json(posts);
+    const post = await Post.find({ userId });
+    res.status(200).json(post);
   } catch (err) {
-    res.status(404).json({ error: err.message });
+    res.status(404).json({ message: err.message });
   }
 };
 
